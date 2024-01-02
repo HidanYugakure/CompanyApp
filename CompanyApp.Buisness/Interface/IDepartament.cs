d2c5d51abac2d44f64aa9ca94b9c90773b01e4a6
HidanYugakure <fidants@code.edu.az>
2024-01-02 13:51:43 +0400

    Interface created CompanyApp.Businessde (Full) Departament Layey ucun

---
+++ CompanyApp.Buisness/Interface/IDepartament.cs
@@ -0,0 +1,22 @@
+﻿using CompanyApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyApp.Buisness.Interface
+{
+    public interface IDepartament
+    {
+        Department Create(Department department);
+        Department Update(int id,Department department);
+        Department Delete(int id);
+
+        Department Get(int id);
+        List<Department> GetAll(string name);
+        List<Department> GetAll(int capacity);
+        List <Department> GetAll();
+
+    }
+}
