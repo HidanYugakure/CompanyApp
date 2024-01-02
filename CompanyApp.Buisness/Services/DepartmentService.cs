2d256933baa0b3a716365a7485d5c14bb9fc6867
HidanYugakure <fidants@code.edu.az>
2024-01-02 14:32:25 +0400

    Department servise classinda methodlar yazildi

---
+++ CompanyApp.Buisness/Services/DepartmentService.cs
@@ -0,0 +1,63 @@
+﻿using CompanyApp.Buisness.Interface;
+using CompanyApp.DataContext.Repositories;
+using CompanyApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyApp.Buisness.Services
+{
+    public class DepartmentService : IDepartament
+    {
+        private readonly DepartmentRepositories _departmentRepositories;
+        private readonly EmployeeRepositories _employeeRepositories;
+        private int Count = 1;
+
+        public Department Create(Department department)
+        {
+         Department existDepartmentWithName = _departmentRepositories.Get(d=>d.Name.Equals(department.Name,StringComparison.OrdinalIgnoreCase));
+         if (existDepartmentWithName is not null) return null;
+         department.Id = Count;
+            if (_departmentRepositories.Create(department))
+            {
+                Count++;
+                return department;
+            }
+            return null;
+        }
+
+        public Department Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department Get(int id)
+        {
+            Department existDepartment = _departmentRepositories.Get(d => d.Id == id);
+            if (existDepartment is null) return null;
+            return existDepartment;
+        }
+
+        public List<Department> GetAll(string name)
+        {
+          return _departmentRepositories.GetAll(d => d.Name == name);
+        }
+
+        public List<Department> GetAll(int capacity)
+        {
+            return _departmentRepositories.GetAll(d => d.Capacity == capacity);
+        }
+
+        public List<Department> GetAll()
+        {
+            return _departmentRepositories.GetAll();
+        }
+
+        public Department Update(int id, Department department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
