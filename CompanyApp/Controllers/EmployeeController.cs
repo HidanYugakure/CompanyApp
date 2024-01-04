ce288d87ce4c741d1e0d1c7eebd43be4a4b00b91
HidanYugakure <fidants@code.edu.az>
2024-01-04 12:44:01 +0400

    Employee Services bolmesindeki 9 metoddan 7 si yaradildi

--- CompanyApp/Controllers/EmployeeController.cs
+++ CompanyApp/Controllers/EmployeeController.cs
@@ -1,13 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CompanyApp.Buisness.Interface;
+using CompanyApp.Buisness.Services;
+using CompanyApp.Domain.Models;
+using CompanyApp.Helpers;
+
 
 namespace CompanyApp.Controllers
 {
     public class EmployeeController
     {
-
+        private readonly EmployeeService employeeServices;
+        public void CreateEmployee()
+        {
+            Helper.changeTextColor("enter Employee Name", ConsoleColor.Magenta);
+            string name = Console.ReadLine();
+            Helper.changeTextColor("enter Employee LastName", ConsoleColor.Magenta);
+            string Surname = Console.ReadLine();
+            Helper.changeTextColor("enter Department", ConsoleColor.Green);
+            string Departamentname = Console.ReadLine();
+            Employee employee = new();
+            employee.Name = name;
+            employee.LastName = Surname;
+            //_employeeService.Create(employee, Departamentname);
+            if (employeeServices.Create(employee, Departamentname) is null)
+            {
+                Helper.changeTextColor("went wrong", ConsoleColor.Red);
+            }
+            else
+            {
+                Helper.changeTextColor("succesfully created",ConsoleColor.Red);
+            }
+        }
+        public void EmployeeDelete()
+        {
+            Helper.changeTextColor("enter Id", ConsoleColor.Magenta);
+            int id = int.Parse(Console.ReadLine());
+            var result = employeeServices.Delete(id);
+            if (result == null)
+            {
+                Helper.changeTextColor("something went wrong", ConsoleColor.Red);
+            }
+            else
+            {
+                Helper.changeTextColor("employee deleted", ConsoleColor.Red);
+            }
+        }
+        public void EmployeUpdate()
+        {
+            Helper.changeTextColor("Enter Id", ConsoleColor.Blue);
+            int id = int.Parse(Console.ReadLine());
+            Helper.changeTextColor("enter new Employee Name", ConsoleColor.Blue);
+            string name = Console.ReadLine();
+            Helper.changeTextColor("enter new Employee LaStName", ConsoleColor.Blue);
+            string Surname = Console.ReadLine();
+            Helper.changeTextColor("Enter Departament Name", ConsoleColor.Green);
+            string Departamentname = Console.ReadLine();
+            Employee employee = new();
+            employee.Name = name;
+            employee.LastName = Surname;
+            //employeeServices.Create(employee, departmentname);
+            if (employeeServices.Update(id, employee, Departamentname) is null)
+            {
+                Helper.changeTextColor("went wrong", ConsoleColor.Red);
+            }
+            else
+            {
+                Helper.changeTextColor("succesfully updated", ConsoleColor.Green);
+            }
+        }
+        public void  Get ()
+        {
+            Helper.changeTextColor("Employee by Id :", ConsoleColor.Yellow);
+            int Id = int.Parse(Console.ReadLine());
+            Employee employee = employeeServices.Get(Id);
+            if (employee != null)
+            {
+                Helper.changeTextColor($"{employee.Id}, {employee.Name}", ConsoleColor.Yellow);
+            }
+            else
+            {
+                Helper.changeTextColor("empty", ConsoleColor.Red);
+            }
+        }
+        public void GetAllWithByName()
+        {
+            Helper.changeTextColor("Employee list : Name", ConsoleColor.Red);
+            List<Employee> employee = employeeServices.GetAll();
+            foreach (Employee employees in employee)
+            {
+                Helper.changeTextColor($"{employees.Id},{employees.Name}", ConsoleColor.Yellow);
+            }
+        }
+        public void GetAllWithByAge()
+        {
+            Helper.changeTextColor("Employee list: Age", ConsoleColor.Green);
+            List<Employee> employee = employeeServices.GetAll();
+            foreach(Employee employees in employee)
+            {
+                Helper.changeTextColor($"{employees.Id},{employees.Name},{ employees.Age}", ConsoleColor.Yellow);
+            }
+        }
+        public void GetWithByDepartmentId() 
+        {
+            Helper.changeTextColor("Enter Departament Id:", ConsoleColor.Green);
+            string DepartamentId = Console.ReadLine();
+            var something = employeeServices.GetEmployeeDepartmentId(DepartamentId);
+            foreach (var item in something)
+            {
+                Console.WriteLine($"id:{item.Id}Name:{item.Name} ");
+            }
+        }
     }
 }
--- CompanyApp/Program.cs
+++ CompanyApp/Program.cs
@@ -1,17 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CompanyApp.Buisness.Interface;
+using CompanyApp.Domain.Models;
 using CompanyApp.Helpers;
 
-Console.WriteLine("Hello, World!");
+Department department = new Department();
+
 while (true)
 {
-Enter: string choice = Console.ReadLine();
-    int result;
-    bool isConvertable = int.TryParse(choice, out result);
+    Helper.changeTextColor("1. CreateDepartment\n" +
+                        "2. GetAllDepartament\n" +
+                        "3. GetDepartmentById\n" +
+                        "4. GetAllDepartmentByCapacity \n" +
+                        "5. UpdateDepartment\n" +
+                        "6. DeleteDepartment\n", ConsoleColor.Magenta);
 
-    switch (result)
-    {
+      Enter: string choice = Console.ReadLine();
+      int result;
+      bool isConvertable = int.TryParse(choice, out result);
+
+      switch (result)
+      {
+        case (int)Catalog.CreateDepartment:
+        
         default:
-            Helper.changeTextColor("Wrong Option", ConsoleColor.Red);
+            Helper.changeTextColor("Wrong", ConsoleColor.Red);
             goto Enter;
-    }
+      }
 }
+
